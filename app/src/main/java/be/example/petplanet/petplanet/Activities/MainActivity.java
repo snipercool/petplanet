6b2e9038e4eddb2ea5ed428f4d9f95f4f05faaf3
NicolasV <vanhuynegem.n@hotmail.com>
2019-05-10 21:18:23 +0200

    qr code scanner

---
+++ app/src/main/java/be/example/petplanet/petplanet/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package be.example.petplanet.petplanet.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import be.example.petplanet.petplanet.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
