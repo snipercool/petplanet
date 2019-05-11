cbf64fb04dbc52c9fcab6d34011701c7a29973ad
haege <haegeman.irene@hotmail.com>
2019-05-11 14:08:34 +0200

    Add sign up, sign in and logout

--- app/src/main/java/be/example/petplanet/petplanet/Activities/MainActivity.java
+++ app/src/main/java/be/example/petplanet/petplanet/Activities/MainActivity.java
@@ -1,15 +1,141 @@
 package be.example.petplanet.petplanet.Activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Arrays;
 
 import be.example.petplanet.petplanet.R;
 
+//Authenticatie code voor gebruiker
+
 public class MainActivity extends AppCompatActivity {
+
+    // Constanten
+
+    public static final String ANONYMOUS = "anonymous";
+    public static final int RC_SIGN_IN = 1;
+
+    // Firebase - algemeen
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mUniqueReference;
+
+    // Firebase - authentication
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private String mUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Firebase - initializeren van algemene variabelen.
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+
+        // Firebase - initializeren van authenticatie variabelen.
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        // Inloggen
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                // this firebaseAuth contains whether at that moment, the user is authenticated or not
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // inloggen
+                    onSignedInInitialize(user.getDisplayName());
+                } else {
+                    // uitloggen
+                    onSignedOutCleanup();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                            //new AuthUI.IdpConfig.FacebookBuilder().build(),
+                                            new AuthUI.IdpConfig.EmailBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+    }
+
+    // Menu
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                // uitloggen
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //Een methode om na te gaan of de activiteit al bestond met de bijbehorende parameters.
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_CANCELED) {
+                finish();
+            }
+        }
+    }
+
+    //Activity LifyCycle
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mAuthStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    //In- en uitloggen
+
+    private void onSignedInInitialize(String username) {
+        mUsername = username;
+    }
+
+    private void onSignedOutCleanup() {
+        mUsername = ANONYMOUS;
     }
 }
