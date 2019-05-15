18f1a836b32efc6d0815e1cc5a95dc257693088e
haege <haegeman.irene@hotmail.com>
2019-05-15 13:27:52 +0200

    Bugfix: notification

--- app/src/main/java/be/example/petplanet/petplanet/Activities/MainActivity.java
+++ app/src/main/java/be/example/petplanet/petplanet/Activities/MainActivity.java
@@ -1,11 +1,13 @@
 package be.example.petplanet.petplanet.Activities;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -22,6 +24,7 @@ import java.util.Arrays;
 import be.example.petplanet.petplanet.R;
 
 import android.app.NotificationManager;
+import android.app.NotificationChannel;
 import android.app.PendingIntent;
 import android.support.v4.app.NotificationCompat;
 
@@ -52,6 +55,12 @@ public class MainActivity extends AppCompatActivity {
     // Score
     private int score;
 
+    //Notification
+    public static final String CHANNEL_1_ID = "channel1";
+    public static final String CHANNEL_2_ID = "channel2";
+    public static final String CHANNEL_3_ID = "channel3";
+    private NotificationManagerCompat notificationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,6 +73,9 @@ public class MainActivity extends AppCompatActivity {
         // Firebase - initializeren van authenticatie variabelen.
 
         mFirebaseAuth = FirebaseAuth.getInstance();
+
+        //Notification
+        notificationManager = NotificationManagerCompat.from(this);
 
         // Inloggen
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
@@ -90,6 +102,8 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+
+        createNotificationChannel();
 
         //Test tot optimalisatie homescreen
         Button btn_notification = findViewById(R.id.btn_notification);
@@ -166,69 +180,76 @@ public class MainActivity extends AppCompatActivity {
     private void addNotification() {
 
         /*Pas volgende code aan volgens de score die bepaald is bij user story 3!!!*/
-        score = -5;
+        score = 52;
+
         //Planeet = slecht
         if (score <= 0) {
-
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                    .setSmallIcon(R.mipmap.ic_launcher_round)
-                    .setContentTitle("Your planet is dying.")
-                    .setContentText("Oh no! Your planet is at the verge of destruction. You can still save it by taking action.")
-                    .setAutoCancel(true);
-
-            // Intent
-            Intent notificationIntent = new Intent(this, MainActivity.class);
-            PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-            builder.setContentIntent(contentIntent);
-
-            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            manager.notify(0, builder.build());
+            Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Your planet is dying.")
+                .setContentText("Oh no! Your planet score is getting low.")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setAutoCancel(true)
+                .build();
+ 
+            notificationManager.notify(1, notification);
         }
-        //Planeet = oké
+        //Planeet = ok
         else if (score > 0 & score <= 50) {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                    .setSmallIcon(R.mipmap.ic_launcher_round)
-                    .setContentTitle("Everything is going okay.")
-                    .setContentText("Nothing went wrong nor good but don't forget to take care.");
+            Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Everything is going okay.")
+                .setContentText("Nothing went wrong nor good but don't forget to take care.")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setAutoCancel(true)
+                .build();
 
-            // Intent
-            Intent notificationIntent = new Intent(this, MainActivity.class);
-            PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-            builder.setContentIntent(contentIntent);
-
-            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            manager.notify(0, builder.build());
+            notificationManager.notify(2, notification);
         }
         //Planeet = goed
         else {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                    .setSmallIcon(R.mipmap.ic_launcher_round)
-                    .setContentTitle("Everybody is happy.")
-                    .setContentText("Well done! Your planet has never been alive as it is now.");
+            Notification notification = new NotificationCompat.Builder(this, CHANNEL_3_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Your planet is full of hapiness.")
+                .setContentText("Nothing went wrong nor good yet don't forget to take care.")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setAutoCancel(true)
+                .build();
 
-            // Intent
-            Intent notificationIntent = new Intent(this, MainActivity.class);
-            PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-            builder.setContentIntent(contentIntent);
-
-            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            manager.notify(0, builder.build());
+            notificationManager.notify(3, notification);
         }
     }
 
     private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.channel_name);
-            String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
+            NotificationChannel channel1 = new NotificationChannel(
+                    CHANNEL_1_ID,
+                    "Channel 1",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel1.setDescription("This is Channel 1");
+
+            NotificationChannel channel2 = new NotificationChannel(
+                    CHANNEL_2_ID,
+                    "Channel 2",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel2.setDescription("This is Channel 2");
+
+            NotificationChannel channel3 = new NotificationChannel(
+                    CHANNEL_3_ID,
+                    "Channel 3",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel2.setDescription("This is Channel 3");
+ 
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+            manager.createNotificationChannel(channel2);
+            manager.createNotificationChannel(channel3);
         }
     }
 }
