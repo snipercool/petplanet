773d83a811956fc1db52058946eba051d6fd063e
haege <haegeman.irene@hotmail.com>
2019-05-18 19:55:08 +0200

    Add setters and getters

---
+++ app/src/main/java/be/example/petplanet/petplanet/Activities/TemperatureClass.java
@@ -0,0 +1,35 @@
+package be.example.petplanet.petplanet.Activities;
+
+public class TemperatureClass {
+    int xValue;
+    int yValue;
+
+    //Constructor
+    public TemperatureClass(){
+
+    }
+
+    //Setters
+
+    public void setxValue(int value){
+        this.xValue = value;
+    }
+
+    public void setyValue(int value){
+        this.yValue = value;
+    }
+
+    //Getters
+
+    public int getxValue(){
+        return xValue;
+    }
+
+    public int getyValue(){
+        return yValue;
+    }
+
+    public void PointValue(){
+
+    }
+}
