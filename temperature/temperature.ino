ad569e188f3d324964e7092ab1918151fe80b39e
haege <haegeman.irene@hotmail.com>
2019-05-26 15:53:39 +0200

    Add date of when temperature was found

--- temperature/temperature.ino
+++ temperature/temperature.ino
@@ -2,6 +2,8 @@
 #include <FirebaseArduino.h>                                                // firebase library
 #include <ESP8266WiFi.h>                                                    // esp8266 library
 #include <DHT.h>                                                            // dht11 temperature and humidity sensor library
+#include <WiFiUdp.h>
+#include <NTPClient.h>
 
 #define WIFI_SSID "NAME_OF_WIFI"                                             // input your home or public wifi name 
 #define WIFI_PASSWORD "PASSWORD"                                    //password of wifi ssid
@@ -12,6 +14,14 @@
 #define DHTPIN 2                                                           // what digital pin we're connected to
 #define DHTTYPE DHT11                                                       // select dht type as DHT 11 or DHT22
 DHT dht(DHTPIN, DHTTYPE);                                                     
+
+//Date
+const long utcOffsetInSeconds = 3600;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 void setup() {
   Serial.begin(9600);
@@ -30,9 +40,14 @@ void setup() {
   Serial.println(WiFi.localIP());                                            //print local IP address
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);                              // connect to firebase
   dht.begin();                                                               //Start reading dht sensor
+  timeClient.begin();
 }
 
 void loop() {
+  //Daytime
+  timeClient.update();
+  Serial.print(daysOfTheWeek[timeClient.getDay()]);
+  
   float h = dht.readHumidity();                                              // Reading temperature or humidity takes about 250 milliseconds!
   float t = dht.readTemperature();                                           // Read temperature as Celsius (the default)
     
@@ -45,10 +60,13 @@ void loop() {
   String fireHumid = String(h) + String("%");                                         //convert integer humidity to string humidity 
   Serial.print("%  Temperature: ");  Serial.print(t);  Serial.println(" °C ");
   String fireTemp = String(t) + String(" °C");                                                     //convert integer temperature to string temperature
-  delay(600000); //every 10 minutes
   
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
-  root["temperature"] = fireTemp;
-  Firebase.pushString("/sensors/temperature", fireTemp);
+  root["temperature"] = t;
+  root["day"] = daysOfTheWeek[timeClient.getDay()];
+  Firebase.push("/sensors/temperature", root);
+  root.printTo(Serial);
+
+  delay(600000); //every 10 minutes
 }
