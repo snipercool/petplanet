475b0f28642bb3182d3325104427ebc55421bfdf
haege <haegeman.irene@hotmail.com>
2019-05-28 11:43:29 +0200

    Create files
    Desc:
    - activity_planetinfo.xml
    - PlanetinfoActivity.java

---
+++ app/src/main/java/be/example/petplanet/petplanet/Activities/PlanetinfoActivity.java
@@ -0,0 +1,14 @@
+package be.example.petplanet.petplanet.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import be.example.petplanet.petplanet.R;
+
+public class PlanetinfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planetinfo);
+    }
+}
