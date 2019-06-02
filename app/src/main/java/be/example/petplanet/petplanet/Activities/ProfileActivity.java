bc22fa93913a9a4eae22ac1156c6bc48b66ae084
snipercool <sniper.cool@hotmail.com>
2019-06-02 21:19:36 +0200

    fix profile layout, logout on profile

--- app/src/main/java/be/example/petplanet/petplanet/Activities/ProfileActivity.java
+++ app/src/main/java/be/example/petplanet/petplanet/Activities/ProfileActivity.java
@@ -2,12 +2,22 @@ package be.example.petplanet.petplanet.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import be.example.petplanet.petplanet.R;
 
@@ -21,11 +31,40 @@ public class ProfileActivity extends AppCompatActivity {
     ImageButton graphs;
     ImageButton qrcode;
     ImageButton leaderbord;
+    TextView scoring;
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mUniqueReference;
+    private DatabaseReference mPlanetReference;
+    private DatabaseReference mScoreReference;
+
+    // Firebase - authentication
+
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        // Firebase - initializeren van algemene variabelen.
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+
+        // Firebase - initializeren van authenticatie variabelen.
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        // Firebase - initializeren van referentie.
+
+        mUniqueReference = mFirebaseDatabase.getReference();
+
+
+        //get score
+        mPlanetReference = mFirebaseDatabase.getReference().child("planet");
+        mScoreReference = mPlanetReference.child("0").child("score");
+
+        scoring = (TextView) findViewById(R.id.score);
 
         // go to planet
         planet = findViewById(R.id.btn_menu_planet);
@@ -57,6 +96,19 @@ public class ProfileActivity extends AppCompatActivity {
                 Intent intent;
                 intent = new Intent(ProfileActivity.this, StatsActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        mScoreReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String score = dataSnapshot.getValue().toString();
+                scoring.setText("Score= "+ score);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
