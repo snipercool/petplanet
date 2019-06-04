87fbef47e98c2a5ce3c542edd89a3dabedf473e3
haeirene <haegeman@hotmail.com>
2019-06-04 10:37:18 +0200

    Bugfix menu leaderboardActivity
    Desc:
    - The menu icons where there but just there as they didn't link to the other pages. This has been fixed so the user can navigate through the application.

--- app/src/main/java/be/example/petplanet/petplanet/Activities/LeaderboardActivity.java
+++ app/src/main/java/be/example/petplanet/petplanet/Activities/LeaderboardActivity.java
@@ -1,14 +1,57 @@
 package be.example.petplanet.petplanet.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 
 import be.example.petplanet.petplanet.R;
 
 public class LeaderboardActivity extends AppCompatActivity {
+    private ImageButton planet;
+    private ImageButton solar;
+    private ImageButton graphs;
+    private ImageButton qrcode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+
+        // go to planetinfo
+        planet = findViewById(R.id.planet);
+        planet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(LeaderboardActivity.this, PlanetinfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // go to qrcode
+        qrcode = findViewById(R.id.qrcode);
+        qrcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(LeaderboardActivity.this, ScannerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // go to grafieken
+        graphs = findViewById(R.id.graphs);
+        graphs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(LeaderboardActivity.this, StatsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
